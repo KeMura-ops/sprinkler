c808bdf3ed6ba28adede5974621e18bef83a3da1
KeMura-ops <kenti432@yahoo.co.jp>
2021-05-28 02:12:40 +0900

    Ajaxの記述だが、何故か動作しなかったので一応履歴として残す

--- app/controllers/nices_controller.rb
+++ app/controllers/nices_controller.rb
@@ -1,18 +1,18 @@
 class NicesController < ApplicationController
+  before_action :nice_params
   before_action :authenticate_user!
 
   def create
     Nice.create(user_id: current_user.id, post_id: params[:id])
-    redirect_to timeline_posts_path
   end
 
   def destroy
     Nice.find_by(user_id: current_user.id, post_id: params[:id]).destroy
-    redirect_to timeline_posts_path
   end
 
   private
-    def nice_params
-      params.permit(:post_id)
-    end
+
+  def nice_params
+    @post = Post.find(params[:id])
+  end
 end
