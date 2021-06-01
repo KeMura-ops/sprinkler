3120b5358c4e4ea6d21bb355463bb1d6fabb5916
KeMura-ops <kenti432@yahoo.co.jp>
2021-06-01 15:22:32 +0900

    postモデルの単体テストコード

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  before do
+    @post = FactoryBot.build(:post)
+  end
+  describe '投稿の保存' do
+    context '画像投稿ができる場合' do
+      it '画像とテキストがあれば投稿できる' do
+        expect(@post).to be_valid
+      end
+      it 'テキストがなくても画像が存在すれば投稿できる' do
+        @post.text = nil
+        expect(@post).to be_valid
+      end
+
+      context '画像投稿ができない場合' do
+        it '画像が存在しない場合、投稿できない' do
+          @post.image = nil
+          @post.valid?
+          expect(@post.errors.full_messages).to include("Image can't be blank")
+        end
+      end
+    end
+  end
+end
