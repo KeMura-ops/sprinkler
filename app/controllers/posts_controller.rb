e65a9615b61e40cb6ecfdd5095da6437212dda69
KeMura-ops <kenti432@yahoo.co.jp>
2021-06-09 23:41:40 +0900

    一部変更とレスポンシブ化

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @posts = Post.includes(:tags).limit(10).order(created_at: :desc)
