33fbbdef8220125cd12aa5b8004d27655a27bd11
KeMura-ops <kenti432@yahoo.co.jp>
2021-06-16 19:21:40 +0900

    フォーム修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,14 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :nices
   has_many :comments
-
-  with_options presence: true do
-    validates :nickname, length: { maximum: 10 }
-  end
-
   has_many :active_relationships, class_name: 'Follow', foreign_key: 'user_id'
   has_many :passive_relationships, class_name: 'Follow', foreign_key: 'target_user_id'
   has_many :followings, through: :active_relationships, source: :target_user
   has_many :followers, through: :passive_relationships, source: :user
+
+  with_options presence: true do
+    validates :nickname, length: { maximum: 20 }
+  end
 
   def update_without_current_password(params, *options)
 
@@ -24,7 +23,7 @@ class User < ApplicationRecord
       params.delete(:password_confirmation)
     end
 
-    result = update!(params, *options) #Rails 6.1以降ではupdate_attributes非推奨ということで、update!を使用。
+    result = update(params, *options) #Rails 6.1以降ではupdate_attributes非推奨ということで、updateを使用。
     clean_up_passwords
     result
   end
